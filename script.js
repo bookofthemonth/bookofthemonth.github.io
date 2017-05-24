9176393b23eb812bb911da11d85e4feae158a034
bookofthemonth <extern@fschneider.info>
2017-05-24 21:48:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $.get("data.json", function (resp) {
     $(".title").html(resp.title);
     $(".author").html("by " + resp.author);
-    $(".rating").html( Array(resp.rating).join('<i class="fa fa-star" aria-hidden="true"></i>') );
+    $(".rating").html( Array(resp.rating + 1).join('<i class="fa fa-star" aria-hidden="true"></i>') );
     $(".desc").html(resp.desc);
   })
 });
