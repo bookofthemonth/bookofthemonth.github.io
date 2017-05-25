d0181bbd5930d2e86b41c1e37f42cf2bbd7d6ff4
Anton Felix Lorenzen <anfelor@posteo.de>
2017-05-25 08:35:49 +0200

    Added multiple sections

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-$(document).ready(function () {
-  $.get("data.json", function (resp) {
-    $(".title").html(resp.title);
-    $(".author").html("by " + resp.author);
-    $(".rating").html( Array(resp.rating + 1).join('<i class="fa fa-star" aria-hidden="true"></i>') );
-    $(".desc").html(resp.desc);
-  })
+var injectData = function(topic) {
+    $.get("data.json", function(resp) {
+        var r = resp[topic];
+        $(".title").html(r.title);
+        $(".author").html("by " + r.author);
+        $(".rating").html( Array(r.rating + 1).join('<i class="fa fa-star" aria-hidden="true"></i>') );
+        $(".desc").html(r.desc);
+    })
+};
+
+var reloadPage = function() {
+    var path = window.location.hash;
+    if (path == "#" || path == "") {
+        $(".orgInfo").show();
+        $(".bookInfo").hide();
+    } else {
+        $(".bookInfo").show();
+        $(".orgInfo").hide();
+        injectData(path.slice(1)); // Remove leading hash sign
+    }
+};
+
+$(window).on('hashchange', function() {
+    reloadPage();
+});
+
+$(document).ready(function() {
+    reloadPage();
 });
